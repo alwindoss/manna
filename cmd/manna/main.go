ad783b150333f977f22e5ee794d163344cf02404
Alwin Doss <alwindoss84@gmail.com>
2024-04-06 10:54:15 +0530

    replaced Makefile with Taskfile
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- cmd/manna/main.go
+++ cmd/manna/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/alwindoss/manna/internal/engine"
@@ -10,5 +11,6 @@ func main() {
 	cfg := engine.Config{
 		Port: 3030,
 	}
+	fmt.Println("starting manna on port ", cfg.Port)
 	log.Fatal(engine.Run(&cfg))
 }
