8cc63a52ae863cbe59d65307b66aef95d71f5613
Alwin Doss <alwindoss84@gmail.com>
2025-07-26 22:51:15 +0530

    add tailwindcss and daisyui for building ui components
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -11,6 +12,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
